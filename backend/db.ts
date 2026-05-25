4ed7a10ddc3cf1f23dac064b0bde715609cf77f2
Hamza-Bettal <battalhamza17@gmail.com>
2026-05-25 16:12:30 +0100

    merged

--- backend/db.ts
+++ backend/db.ts
@@ -2,6 +2,15 @@ import { createClient } from '@libsql/client'
 import 'dotenv/config'
 
 export const db = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!
+    url: process.env.TURSO_DATABASE_URL ?? 'file:local.db',
+    authToken: process.env.TURSO_AUTH_TOKEN
 })
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+`)
